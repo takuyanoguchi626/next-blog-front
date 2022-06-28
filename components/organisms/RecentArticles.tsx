029c64c94ceec62d5cab886908fd6333fd2291f1
noguchitakuya <takuya.noguchi@rakus-partners.co.jp>
2022-06-28 12:34:05 +0900

    dd

--- components/organisms/RecentArticles.tsx
+++ components/organisms/RecentArticles.tsx
@@ -1,14 +1,40 @@
 import { format } from "date-fns";
+import { useState } from "react";
 import { Article } from "../../types/Article";
 
 export const RecentArticle = () => {
-  const recentArticleList: Article[] = [];
+  const [recentArticleList, setRecentArticleList] = useState<Article[]>([
+    {
+      id: 1,
+      title: "サンプル記事",
+      postDate: new Date(),
+      editDate: new Date(),
+      content:
+        "これはサンプルの記事です。SSGの実装が完了したらDBの記事が表示されるようになります。",
+    },
+    {
+      id: 1,
+      title: "サンプル記事",
+      postDate: new Date(),
+      editDate: new Date(),
+      content:
+        "これはサンプルの記事です。SSGの実装が完了したらDBの記事が表示されるようになります。",
+    },
+    {
+      id: 1,
+      title: "サンプル記事",
+      postDate: new Date(),
+      editDate: new Date(),
+      content:
+        "これはサンプルの記事です。SSGの実装が完了したらDBの記事が表示されるようになります。",
+    },
+  ]);
   return (
     <>
       <h1>最近の投稿</h1>
       <div>
         {recentArticleList.map((article) => {
-          const formatPostDate = format(new Date(), "yyyy/MM/dd");
+          const formatPostDate = format(article.postDate, "yyyy/MM/dd");
           return (
             <>
               <div>{article.title}</div>
